c6f73070c33b14a796654fe561b4fc94f0e0b3ee
dblem <dax.blem@gmail.com>
2016-02-23 01:43:35 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,35 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+
+  def initialize(input=nil)
+    input = input.to_i
+    @range = Array.new
+    (1..input).each do |x|
+      @range << x
+    end
+  end
+
+  def gcd(x, y)
+    while x != y
+      if x > y
+        x = x - y
+      else
+        y = y - x
+      end
+    end
+    x
+  end
+
+  def two_arg_lcm(x, y)
+    x * y / gcd(x, y)
+  end
+
+  def lcm
+    while @range.length > 2
+      n = two_arg_lcm(@range[0], @range[1])
+      @range.shift
+      @range[0] = n
+    end
+    two_arg_lcm(@range[0], @range[1])
+  end
+
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,32 @@
-# Implement your procedural solution here!
+def gcd(x, y)
+  while x != y
+    if x > y
+      x = x - y
+    else
+      y = y - x
+    end
+  end
+  x
+end
+
+def lcm(x, y)
+  x * y / gcd(x, y)
+end
+
+def multi_lcm(range)
+  while range.length > 2
+    n = lcm(range[0], range[1])
+    range.shift
+    range[0] = n
+  end
+  lcm(range[0], range[1])
+end
+
+def smallest_multiple(input)
+  input = input.to_i
+  range = Array.new
+  (1..input).each do |x|
+    range << x
+  end
+  multi_lcm(range)
+end
